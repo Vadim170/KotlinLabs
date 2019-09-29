84fcc05dd2f0b06fd82ce32957e03a9a35202bb2
CODE <Vadim.17.0@yandex.ru>
2019-09-29 13:28:05 +0300

    lab5 commit

--- src/Lab2.kt
+++ src/Lab2.kt
@@ -1,5 +1,11 @@
 import kotlin.math.min
 
-fun min(num1: Int, num2: Int, num3: Int, num4: Int) = min(
-            min(num1,num2),
-            min(num3,num4))
+fun min(num1: Int, num2: Int, num3: Int, num4: Int) : Int {
+    return when {
+        (num1 <= num2 && num1 <= num3 && num1 <= num4) -> num1
+        (num2 <= num1 && num2 <= num3 && num2 <= num4) -> num2
+        (num3 <= num1 && num3 <= num2 && num3 <= num4) -> num3
+        (num4 <= num1 && num4 <= num2 && num4 <= num3) -> num4
+        else -> 0
+    }
+}
