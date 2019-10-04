2531dea94230cf8332f72d4267e2af6bc481bb8d
CODE <Vadim.17.0@yandex.ru>
2019-10-04 15:43:42 +0300

    lab4_6 first ccommit

--- src/Lab.kt
+++ src/Lab.kt
@@ -1,12 +1,72 @@
-package ru.tinkoff.lab4_5
+package ru.tinkoff.lab4_6
+
+interface Flyable {
+    fun fly()
+}
+
+interface Walkable {
+    fun walk()
+}
+
+interface Swimable {
+    fun swim()
+}
+
+interface Crawlable {
+    fun crawl()
+}
+
+interface Voiceable {
+    fun voice()
+}
 
 abstract class Pet(val name: String, val age: Int)
 
-class Dog(name: String,age: Int) : Pet(name,age)
-class Cat(name: String,age: Int) : Pet(name,age)
-class Parrot(name: String,age: Int) : Pet(name,age)
-class Snake(name: String,age: Int) : Pet(name,age)
-class Fish(name: String,age: Int) : Pet(name,age)
+class Dog(name: String,age: Int) : Pet(name,age), Voiceable, Walkable {
+    override fun voice() {
+        println("Гав")
+    }
+
+    override fun walk() {
+        println("Ар-р-р")
+    }
+}
+
+class Cat(name: String,age: Int) : Pet(name,age), Voiceable, Walkable {
+    override fun voice() {
+        println("Мяу")
+    }
+
+    override fun walk() {
+        println("Кс-с-с")
+    }
+}
+
+class Parrot(name: String,age: Int) : Pet(name,age), Flyable, Voiceable {
+    override fun fly() {
+        println("Вжух-вжух")
+    }
+
+    override fun voice() {
+        println("Привет")
+    }
+}
+
+class Snake(name: String,age: Int) : Pet(name,age), Swimable, Crawlable {
+    override fun swim() {
+        println("Бульк")
+    }
+
+    override fun crawl() {
+        println("Чшшшш")
+    }
+}
+
+class Fish(name: String,age: Int) : Pet(name,age), Swimable {
+    override fun swim() {
+        println("Журк-журк")
+    }
+}
 
 fun main(args: Array<String>){
     val dog = Dog("Bobik", 3)
