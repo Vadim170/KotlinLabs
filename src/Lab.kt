706641ffb39d443d8b8ae78de1e1bd86ded3f070
CODE <Vadim.17.0@yandex.ru>
2019-10-04 19:53:02 +0300

    lab4_8 first commit

--- src/Lab.kt
+++ src/Lab.kt
@@ -1,5 +1,7 @@
 package ru.tinkoff.lab4_6
 
+import java.util.*
+
 interface Flyable {
     fun fly()
 }
@@ -20,7 +22,9 @@ interface Voiceable {
     fun voice()
 }
 
-abstract class Pet(val name: String, val age: Int) {
+abstract class Pet(val name: String, var age: Int) {
+    var isAvailable = true
+
     fun eat() {
         println("Ням-ням")
     }
@@ -76,10 +80,83 @@ class Fish(name: String,age: Int) : Pet(name,age), Swimable {
     }
 }
 
-fun main(args: Array<String>){
-    val dog = Dog("Bobik", 3)
-    val cat = Cat("Mussy", 4)
-    val parrot = Parrot("Mike", 5)
-    val snake = Snake("Smith", 3)
-    val fish = Fish("Nstya", 2)
+class HomePet(var bunchShit: Int, val pet: Pet) {
+
+    fun liveDay(scanner: Scanner) {
+        println("Что хотите сделать с вашим питомцем?")
+        println("(1-Покормить; 2-Убрать за ним)")
+        var action = scanner.nextInt()
+        while (action !in 1..2) {
+            println("Такое действие недоступно, повтори выбор:")
+            action = scanner.nextInt()
+        }
+        when (action) {
+            1 -> {
+                pet.eat()
+                pet.shit()
+                ++bunchShit
+                println("${pet.name} покушал и облегчился")
+            }
+            2 -> {
+                bunchShit = 0
+                println("Вы убрасиль за ${pet.name}")
+                println("${pet.name} уже ${pet.age} дней. Ему надо хорошо питаться")
+            }
+            else -> throw Exception("Указано неизвестное действие")
+        }
+        println(
+            when (bunchShit) {
+                0 -> "Дома чисто и хорошо"
+                1 -> "${pet.name} слегда нагадил, стоит прибраться"
+                2 -> "Как можно жить, когда так грязно? Уберитесь!"
+                3 -> {
+                    pet.isAvailable = false // Вызовет выход из цикла
+                    "${pet.name} не смог жить с таким безолаберным хозяином"
+                }
+                else -> throw Exception("Ошибка в bunch of shit")
+            }
+        )
+        ++pet.age
+    }
 }
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    try {
+        val pet = getPet(scanner)
+        val homePet = HomePet(0, pet)
+        while (homePet.pet.isAvailable) {
+            homePet.liveDay(scanner)
+        }
+    } catch (e: Exception) {
+        println("Произошла ошибка!(${e.message})")
+        println(e.printStackTrace())
+        println(e.toString())
+    }
+}
+
+private fun getPet(scanner: Scanner): Pet {
+    println("Выберите питомца:")
+    println("(1-Собака; 2-Кошка; 3-Попугай; 4-Змея; 5-Рыбка)")
+    var petType = scanner.nextInt()
+    while (petType !in 1..5) {
+        println("Не, таких животных у нас нет(пока)")
+        println("Выберите питомца:")
+        petType = scanner.nextInt()
+    }
+
+    scanner.nextLine()
+    println("Введите имя вашего питомца:")
+    val petName = scanner.nextLine()
+    val petAge = 0
+
+    return when (petType) {
+        1 -> Dog(petName, petAge)
+        2 -> Cat(petName, petAge)
+        3 -> Parrot(petName, petAge)
+        4 -> Snake(petName, petAge)
+        5 -> Fish(petName, petAge)
+        else -> throw Exception("Указан неверный тип животного")
+    }
+}
+
