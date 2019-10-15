1bcfe372517b7ede9f74593b1b9d795fbf96a16e
CODE <Vadim.17.0@yandex.ru>
2019-10-15 18:20:25 +0300

    lab7_4 change one

--- src/Lab.kt
+++ src/Lab.kt
@@ -8,11 +8,20 @@ fun main(args: Array<String>) {
         sex = Sex.FEMALE
     }
     println(pet)
-    println(if pet.isAddult() "Совершеннолетняя" else "Несовершеннолетняя")
+    println(if(pet.isAddult()) "Совершеннолетняя" else "Несовершеннолетняя")
     println(pet.respectableNickname)
 
     val dog = Dog()
-    dog.respectableNickname
+    dog.nickname = "Bobik"
+    println(dog.respectableNickname)
+
+    val dog2 : Pet = Dog()
+    dog2.nickname = "Bobik"
+    println(dog2.respectableNickname)
+
+    // У dog2 вызовется свойство расширения от Pet
+    // и напишет просто "Dear name", а не "Dear dog name"
+    // Это может вызывать путанницу
 }
 
 enum class Sex {
@@ -29,12 +38,15 @@ open class Pet {
     }
 }
 
-class Dog : Pet {
-
-}
+class Dog : Pet()
 
 fun Pet.isAddult() = (age >= 2)
 
-var Pet.respectableNickname
+var Pet.respectableNickname: String
     get() = "Dear $nickname"
+    set(value) {}
+
+var Dog.respectableNickname: String
+    get() = "Dear dog $nickname"
+    set(value) {}
 
