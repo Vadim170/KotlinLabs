166543c6d1310f8ca4f66e2b524bb76bbebbb120
CODE <Vadim.17.0@yandex.ru>
2019-10-15 15:54:47 +0300

    lab7_3 init

--- src/Lab.kt
+++ src/Lab.kt
@@ -1,4 +1,16 @@
-package ru.tinkoff.lab6_8
+package ru.tinkoff.lab7_3
+
+fun main(args: Array<String>) {
+    val pet = Pet()
+    with(pet) {
+        nickname = "Mary"
+        age = 21
+        sex = Sex.FEMALE
+    }
+    println(pet)
+    println(if pet.isAddult() "Совершеннолетняя" else "Несовершеннолетняя")
+    println(pet.respectableNickname)
+}
 
 enum class Sex {
     MALE, FEMALE
@@ -14,30 +26,8 @@ class Pet {
     }
 }
 
-fun main() {
-    val pet = Pet()
-    pet.apply { // Возвращает объект переданный в аргументе. Это не нужно, поэтому with в той же степени подходит
-        nickname = "Tor"
-        age = 12
-        sex = Sex.MALE
-    }
-    println(pet)
-    with(pet) { // Возвращает результат последнего лямбда-выражения. Это не нужно, поэтому apply в той же степени подходит
-        nickname = "Mary"
-        age = 21
-        sex = Sex.FEMALE
-    }
-    println(pet)
-    pet.also { // "it." это лишние символы, которые можно не писать.
-        it.nickname = "Victor"
-        it.age = 1
-        it.sex = null
-    }
-    println(pet)
-    pet.let { // "it." это лишние символы, которые можно не писать. И let применяют для null опасных типов, здесь это не требуется.
-        it.nickname = "Kistocka"
-        it.age = 2
-        it.sex = null
-    }
-    println(pet)
-}
+fun Pet.isAddult() = (age >= 2)
+
+var Pet.respectableNickname
+    get() = "Dear $nickname"
+
