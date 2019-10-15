5636268271f9cd7985baa1e7f7ebcf759e8f9762
CODE <Vadim.17.0@yandex.ru>
2019-10-15 18:48:19 +0300

    lab7_5 change one

--- src/Lab.kt
+++ src/Lab.kt
@@ -1,18 +1,23 @@
 package ru.tinkoff.lab7_5
 
 fun main(args: Array<String>) {
+    // Создадим хозяина Pet
     val pet = Pet()
     with(pet) {
         nickname = "Mary"
         age = 21
         sex = Sex.FEMALE
     }
-    println(pet)
-    println(if pet.isAddult() "Совершеннолетняя" else "Несовершеннолетняя")
+    println(if(pet.isAddult()) "Совершеннолетняя" else "Несовершеннолетняя")
     println(pet.respectableNickname)
-
+    val owner = Owner(pet)
+    owner.feed()
+    // Создадим хозяина Dog
     val dog = Dog()
-    dog.respectableNickname
+    dog.nickname = "Charly"
+    println(dog.respectableNickname)
+    val dogOwner = Owner(dog)
+    dogOwner.feed()
 }
 
 enum class Sex {
@@ -29,16 +34,17 @@ open class Pet {
     }
 }
 
-class Dog : Pet
+class Dog : Pet()
 
-class Cat : Pet
+class Cat : Pet()
 
 fun Pet.isAddult() = (age >= 2)
 
-var Pet.respectableNickname
+var Pet.respectableNickname: String
     get() = "Dear $nickname"
+    set(value) {}
 
-class <Pet> Owner(val pet: <Pet>) {
+class Owner<T : Pet>(val pet: T) {
     fun feed() = println("Домашнее животное ${pet.nickname} накормлено")
 }
 
